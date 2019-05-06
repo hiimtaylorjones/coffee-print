2c4562a11f66a62ec6430e44ea2a6fc7deb694b4
Taylor Jones <taylor.jones@heroku.com>
2019-05-06 16:32:20 -0400

    trying to get polymorphic working in ember

--- frontend/app/models/provider.js
+++ frontend/app/models/provider.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  ratings: DS.hasMany('ratings', { inverse: 'subject' })
 });
--- frontend/app/models/rating.js
+++ frontend/app/models/rating.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  reviewNotes: DS.attr('string'),
+  ratingValue: DS.attr('number'),
+  subject: DS.belongsTo('subject', { polymorphic: true })
 });
